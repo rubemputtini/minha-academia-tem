33cd25bda9cf1309378eeb1af0cc02763714fce4
Rubem Staciarini Puttini <rstp@vtxrm.com>
2024-10-30 14:18:04 +0000

    Organize Frontend + Backend

--- src/components/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-const ProtectedRoute = ({ children }) => {
-    const token = localStorage.getItem('token');
-
-    return token ? children : <Navigate to="/signup" />;
-};
-
-export default ProtectedRoute;
